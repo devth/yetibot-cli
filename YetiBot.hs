568a4f93e78928bf3c7fb945bd2d2c1566f7971b
Trevor Hartman <trevorhartman@gmail.com>
2013-05-02 16:10:23 -0700

    Include user access token in api calls

--- YetiBot.hs
+++ YetiBot.hs
@@ -6,13 +6,14 @@ import System.IO
 main :: IO ()
 main = do
     base <- getEnv "YETIBOT_ENDPOINT"
+    token <- getEnv "YETIBOT_ACCESS_TOKEN"
     args <- getArgs
     -- optionally read in stdin only if it was piped (i.e. if it's not a terminal
     -- device)
     contents <- hIsTerminalDevice stdin >>= \isT -> if isT then return "" else getContents
-    let url = endpoint base $ intercalate " " $ args ++ [contents]
+    let url = endpoint base token $ intercalate " " $ args ++ [contents]
     resp <- simpleHTTP (postRequest url) >>= getResponseBody
     putStrLn resp
 
-endpoint :: String -> String -> String
-endpoint base = ((base ++ "/api?command=") ++) . urlEncode
+endpoint :: String -> String -> String -> String
+endpoint base token = ((base ++ "/api?token=" ++ token ++ "command=") ++) . urlEncode
