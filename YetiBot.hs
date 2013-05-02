0c7971b2af9c4bb5daafe6008968a4ec7f83479e
Trevor Hartman <trevorhartman@gmail.com>
2013-05-02 15:12:34 -0700

    Remove unnecessary parens

--- YetiBot.hs
+++ YetiBot.hs
@@ -10,7 +10,7 @@ main = do
     -- optionally read in stdin only if it was piped (i.e. if it's not a terminal
     -- device)
     contents <- hIsTerminalDevice stdin >>= \isT -> if isT then return "" else getContents
-    let url = endpoint base $ (intercalate " " $ args ++ [contents])
+    let url = endpoint base $ intercalate " " $ args ++ [contents]
     resp <- simpleHTTP (postRequest url) >>= getResponseBody
     putStrLn resp
 
