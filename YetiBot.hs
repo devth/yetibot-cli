536b714ad2352de106bc3c13d2a7381f9be3139b
Trevor Hartman <trevorhartman@gmail.com>
2013-05-21 14:37:19 -0600

    Prevent extraneous spaces at end of urls

--- YetiBot.hs
+++ YetiBot.hs
@@ -1,19 +1,26 @@
-import Data.List
 import Network.HTTP
 import System.Environment
 import System.IO
+import Data.List (intercalate)
+import Data.Text (strip, pack, unpack)
 
 main :: IO ()
 main = do
     base <- getEnv "YETIBOT_ENDPOINT"
     token <- getEnv "YETIBOT_ACCESS_TOKEN"
     args <- getArgs
+    putStrLn $ show args
     -- optionally read in stdin only if it was piped (i.e. if it's not a terminal
     -- device)
     contents <- hIsTerminalDevice stdin >>= \isT -> if isT then return "" else getContents
-    let url = endpoint base token $ intercalate " " $ args ++ [contents]
-    resp <- simpleHTTP (getRequest url) >>= getResponseBody
-    putStrLn resp
+
+    let argsWithContents = case contents of
+                                [] -> args
+                                c -> args ++ [unpack $ strip $ pack $ contents]
+        url = endpoint base token $ intercalate " " argsWithContents
+    putStrLn $ "url is " ++ url
+    -- resp <- simpleHTTP (getRequest url) >>= getResponseBody
+    -- putStrLn resp
 
 endpoint :: String -> String -> String -> String
 endpoint base token = ((base ++ "/api?token=" ++ token ++ "&command=") ++) . urlEncode
