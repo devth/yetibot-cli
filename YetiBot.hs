c0ea6fe4093793192781bd93d7dcd9e3b1dca0f0
Trevor Hartman <trevorhartman@gmail.com>
2013-05-21 14:45:23 -0600

    Re-enable calls to remote yeti

--- YetiBot.hs
+++ YetiBot.hs
@@ -9,18 +9,15 @@ main = do
     base <- getEnv "YETIBOT_ENDPOINT"
     token <- getEnv "YETIBOT_ACCESS_TOKEN"
     args <- getArgs
-    putStrLn $ show args
     -- optionally read in stdin only if it was piped (i.e. if it's not a terminal
     -- device)
     contents <- hIsTerminalDevice stdin >>= \isT -> if isT then return "" else getContents
-
     let argsWithContents = case contents of
                                 [] -> args
                                 c -> args ++ [unpack $ strip $ pack $ contents]
         url = endpoint base token $ intercalate " " argsWithContents
-    putStrLn $ "url is " ++ url
-    -- resp <- simpleHTTP (getRequest url) >>= getResponseBody
-    -- putStrLn resp
+    resp <- simpleHTTP (getRequest url) >>= getResponseBody
+    putStrLn resp
 
 endpoint :: String -> String -> String -> String
 endpoint base token = ((base ++ "/api?token=" ++ token ++ "&command=") ++) . urlEncode
